86775fbc94be9d1823e55f87a7a3ef2c985872e4
Pete <peteg@hotmail.co.nz>
2026-09-15 17:45:15 +1200

    WI-3: Person roles and identity links (events, folds, decides, tests)

--- tests/Soarscore.Domain.Tests/PersonFoldTests.cs
+++ tests/Soarscore.Domain.Tests/PersonFoldTests.cs
@@ -122,4 +122,130 @@ public class PersonFoldTests
         final.Contact.Should().Be(newContact);
         final.Club.Should().BeNull();
     }
+
+    [Fact]
+    public void RoleGranted_folds_the_role_into_the_set()
+    {
+        var registered = Person.Create(new PersonRegistered(PersonId.New(), "Alex Pilot", SampleContact, SampleClub, DateTimeOffset.UtcNow));
+
+        var granted = registered.Apply(new RoleGranted(PersonRole.Organiser, DateTimeOffset.UtcNow));
+
+        granted.Roles.Should().ContainSingle().Which.Should().Be(PersonRole.Organiser);
+        granted.Id.Should().Be(registered.Id);
+        granted.Name.Should().Be(registered.Name);
+        granted.Contact.Should().Be(registered.Contact);
+        granted.Identities.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void RoleGranted_of_an_already_held_role_folds_to_no_change()
+    {
+        var registered = Person.Create(new PersonRegistered(PersonId.New(), "Alex Pilot", SampleContact, SampleClub, DateTimeOffset.UtcNow));
+        var granted = registered.Apply(new RoleGranted(PersonRole.Competitor, DateTimeOffset.UtcNow));
+
+        var grantedAgain = granted.Apply(new RoleGranted(PersonRole.Competitor, DateTimeOffset.UtcNow));
+
+        grantedAgain.Roles.Should().ContainSingle().Which.Should().Be(PersonRole.Competitor);
+    }
+
+    [Fact]
+    public void RoleRevoked_folds_the_role_out_of_the_set()
+    {
+        var at = DateTimeOffset.UtcNow;
+        var registered = Person.Create(new PersonRegistered(PersonId.New(), "Alex Pilot", SampleContact, SampleClub, at));
+        var granted = registered.Apply(new RoleGranted(PersonRole.Competitor, at));
+
+        var revoked = granted.Apply(new RoleRevoked(PersonRole.Competitor, at));
+
+        revoked.Roles.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void RoleRevoked_of_a_role_not_held_folds_to_no_change()
+    {
+        var registered = Person.Create(new PersonRegistered(PersonId.New(), "Alex Pilot", SampleContact, SampleClub, DateTimeOffset.UtcNow));
+
+        var revoked = registered.Apply(new RoleRevoked(PersonRole.Organiser, DateTimeOffset.UtcNow));
+
+        revoked.Roles.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void IdentityLinked_folds_the_link_into_the_identities()
+    {
+        var registered = Person.Create(new PersonRegistered(PersonId.New(), "Alex Pilot", SampleContact, SampleClub, DateTimeOffset.UtcNow));
+
+        var linked = registered.Apply(new IdentityLinked("auth0", "auth0|12345", DateTimeOffset.UtcNow));
+
+        linked.Identities.Should().ContainSingle().Which.Should().Be(new IdentityLink("auth0", "auth0|12345"));
+        linked.Id.Should().Be(registered.Id);
+        linked.Roles.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void A_duplicated_IdentityLinked_event_folds_to_a_single_link()
+    {
+        var registered = Person.Create(new PersonRegistered(PersonId.New(), "Alex Pilot", SampleContact, SampleClub, DateTimeOffset.UtcNow));
+        var at = DateTimeOffset.UtcNow;
+
+        var linked = registered
+            .Apply(new IdentityLinked("auth0", "auth0|12345", at))
+            .Apply(new IdentityLinked("auth0", "auth0|12345", at));
+
+        linked.Identities.Should().ContainSingle().Which.Should().Be(new IdentityLink("auth0", "auth0|12345"));
+    }
+
+    [Fact]
+    public void RoleGranted_against_no_current_projection_throws()
+    {
+        FluentActions.Invoking(() =>
+            Person.Apply(null, new RoleGranted(PersonRole.Competitor, DateTimeOffset.UtcNow)))
+            .Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void RoleRevoked_against_no_current_projection_throws()
+    {
+        FluentActions.Invoking(() =>
+            Person.Apply(null, new RoleRevoked(PersonRole.Competitor, DateTimeOffset.UtcNow)))
+            .Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void IdentityLinked_against_no_current_projection_throws()
+    {
+        FluentActions.Invoking(() =>
+            Person.Apply(null, new IdentityLinked("auth0", "auth0|12345", DateTimeOffset.UtcNow)))
+            .Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void A_stream_with_roles_and_identity_links_folds_in_order_to_the_expected_final_state()
+    {
+        var id = PersonId.New();
+        var registeredAt = new DateTimeOffset(2026, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        var organiserAt = new DateTimeOffset(2026, 2, 1, 0, 0, 0, TimeSpan.Zero);
+        var competitorAt = new DateTimeOffset(2026, 3, 1, 0, 0, 0, TimeSpan.Zero);
+        var linkedAt = new DateTimeOffset(2026, 4, 1, 0, 0, 0, TimeSpan.Zero);
+        var revokedAt = new DateTimeOffset(2026, 5, 1, 0, 0, 0, TimeSpan.Zero);
+
+        PersonEvent[] stream =
+        [
+            new PersonRegistered(id, "Alex Pilot", SampleContact, SampleClub, registeredAt),
+            new RoleGranted(PersonRole.Organiser, organiserAt),
+            new RoleGranted(PersonRole.Competitor, competitorAt),
+            new IdentityLinked("auth0", "auth0|12345", linkedAt),
+            new IdentityLinked("google-oauth2", "google|67890", linkedAt),
+            new RoleRevoked(PersonRole.Competitor, revokedAt),
+        ];
+
+        var final = stream.Aggregate((Person?)null, Person.Apply);
+
+        final.Should().NotBeNull();
+        final!.Id.Should().Be(id);
+        final.Roles.Should().BeEquivalentTo([PersonRole.Organiser]);
+        final.Identities.Should().HaveCount(2);
+        final.Identities.Should().Contain(new IdentityLink("auth0", "auth0|12345"));
+        final.Identities.Should().Contain(new IdentityLink("google-oauth2", "google|67890"));
+    }
 }
